d1a9e9164a67cd1b33ac8e14c37ba4efe999d2d6
aimG313 <157086365+aimG313@users.noreply.github.com>
2024-07-15 01:09:25 +0600

    day-2/100

---
+++ Day_2/Example/code_7.py
@@ -0,0 +1,6 @@
+a = input("Enter 1st number: ")
+b = input("Enter 2nd number: ")
+
+sum = a + b
+
+print("The summation is:",sum)
---
+++ Day_2/Example/code_8.py
@@ -0,0 +1,6 @@
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+sum = a + b
+
+print("The summation is:",sum)
