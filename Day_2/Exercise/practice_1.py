d509ed21eaa613d75f86694566374e6450ac5240
aimG313 <157086365+aimG313@users.noreply.github.com>
2024-07-15 10:26:04 +0600

    Create practice_1.py

---
+++ Day_2/Exercise/practice_1.py
@@ -0,0 +1,5 @@
+
+
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14 * (radius ** 2)
+print("The area of the circle is:", round(area, 2)) #Round function keeps the value limited after fraction point
